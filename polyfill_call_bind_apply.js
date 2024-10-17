7533a89797500d4f7fa9ed2ccd11fde4f2768057
vthalaya <vthalaya@cisco.com>
2024-10-17 10:26:14 +0530

    polyfill for call bind apply

--- polyfill_call_bind_apply.js
+++ polyfill_call_bind_apply.js
@@ -6,8 +6,51 @@ function sayHello(age, role) {
 
 const bindFunc = sayHello.bind(obj)
 
-console.log(sayHello.call(obj, 20, 'developer'));
-console.log(sayHello.apply(obj, [20, 'developer']));
+// console.log(sayHello.call(obj, 20, 'developer'));
+// console.log(sayHello.apply(obj, [20, 'developer']));
 
-console.log(bindFunc(24, 'tester'))
-console.log(bindFunc(34, 'blogger'))
+// console.log(bindFunc(24, 'tester'))
+// console.log(bindFunc(34, 'blogger'))
+
+/** ====== Bind Polyfill ========  */
+
+let newObj = {
+    name: 'Steve'
+}
+
+let printName = {
+    name: 'Tony',
+    sayHello: function(time) {
+        console.log(`Hello ${this.name} Good ${time}`);
+    }
+}
+
+//let HiFunc = printName.sayHello.bind(newObj);
+
+Object.prototype.myBind = function(myObj, ...args) {
+    myObj.myMethod = this;
+    return function () {
+        myObj.myMethod(...args)
+    }
+}
+
+let HiFunc1 = printName.sayHello.myBind(newObj, 'Morning');
+HiFunc1();
+
+/** ====== call Polyfill ========  */
+
+Object.prototype.myCall = function(myObj, ...args) {
+    myObj.myMethod = this;
+    myObj.myMethod(...args)
+}
+
+printName.sayHello.myCall(newObj, 'After Noon');
+
+/** ====== call Polyfill ========  */
+
+Object.prototype.myApply = function(myObj, ...args) {
+    myObj.myMethod = this;
+    myObj.myMethod(...args)
+}
+
+printName.sayHello.myApply(newObj, ['Evening']);
